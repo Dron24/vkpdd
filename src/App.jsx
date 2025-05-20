ff0c3ef572a05ab867628b3469f7f4329ca5dcf1
Dron24 <stepanoff888@yandex.ru>
2025-05-20 22:57:22 +0500

    исправление отображения панелей(рендеринг)

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import bridge from '@vkontakte/vk-bridge';
 import { View, SplitLayout, SplitCol, ScreenSpinner } from '@vkontakte/vkui';
 import { useActiveVkuiLocation } from '@vkontakte/vk-mini-apps-router';
 
-import { Onewayroad, Home } from './panels';
+import { Home, Onewayroad, Markup, Rules, Tests } from './panels';
 import { DEFAULT_VIEW_PANELS } from './routes';
 
 export const App = () => {
@@ -24,8 +24,13 @@ export const App = () => {
     <SplitLayout>
       <SplitCol>
         <View activePanel={activePanel}>
-          <Home id="home" fetchedUser={fetchedUser} />
-          <Onewayroad id="onewayroad" />
+          <View activePanel={activePanel}>
+            <Home id={DEFAULT_VIEW_PANELS.HOME} fetchedUser={fetchedUser} />
+            <Onewayroad id={DEFAULT_VIEW_PANELS.ONEWAYROAD} />
+            <Markup id={DEFAULT_VIEW_PANELS.MARKUP} />
+            <Rules id={DEFAULT_VIEW_PANELS.RULES} />
+            <Tests id={DEFAULT_VIEW_PANELS.TESTS} />
+          </View>
         </View>
       </SplitCol>
       {popout}
