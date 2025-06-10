b629e3a83cd97ed23c0b6edf26e4b3d501f71770
Dron24 <stepanoff888@yandex.ru>
2025-06-10 11:29:31 +0500

    подключение json

---
+++ src/panels/textbook/TextbookViewer.jsx
@@ -0,0 +1,35 @@
+import { Panel, PanelHeader, PanelHeaderBack, Group, Div, SimpleCell } from "@vkontakte/vkui";
+import { useRouteNavigator, useParams } from "@vkontakte/vk-mini-apps-router";
+import textbookData from "@/assets/textbookData.json";
+
+export const TextbookViewer = ({ id }) => {
+  const navigator = useRouteNavigator();
+  const { section } = useParams(); // получаем параметр из URL, например: "rules", "signs"
+  const data = textbookData[section];
+
+  if (!data) {
+    return (
+      <Panel id={id}>
+        <PanelHeader before={<PanelHeaderBack onClick={() => navigator.back()} />}>
+          Раздел не найден
+        </PanelHeader>
+        <Group>
+          <Div>Такого раздела не существует.</Div>
+        </Group>
+      </Panel>
+    );
+  }
+
+  return (
+    <Panel id={id}>
+      <PanelHeader before={<PanelHeaderBack onClick={() => navigator.back()} />}>
+        {data.title}
+      </PanelHeader>
+      <Group>
+        <Div>
+          <div dangerouslySetInnerHTML={{ __html: data.content }} />
+        </Div>
+      </Group>
+    </Panel>
+  );
+};
